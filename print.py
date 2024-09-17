ed54d211e7077b306e0c7da119fa7b6ad50ee9c1
ushers956 <ushershaninz@gmail.com>
2024-09-17 17:12:30 +1200

    print

---
+++ print.py
@@ -0,0 +1,10 @@
+# print a sentence
+print("hi my name is usher and im a year 8")
+#print a number
+print(22)
+#next line after print
+print("i like basketball \n")
+#pass multiple arguments for printing
+print("hello",22)
+# end arguments for print statement
+print("basketball", end= "*")

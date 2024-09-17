eda905ff3a9812caec8c7b0cc0c766c508858825
ushers956 <ushershaninz@gmail.com>
2024-09-17 17:24:43 +1200

    variables

---
+++ variables.py
@@ -0,0 +1,13 @@
+# createing diffrent variables
+x = 5
+y = "usher"
+print(x)
+print(y)
+
+basketball = "basketball"
+print(basketball)
+
+#user input
+name = input("enter your name: ")
+
+print("\n hello", name, "\nwelcome to my wedsite")

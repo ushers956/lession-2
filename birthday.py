a6bbd01dbeaa9661f96dc8e324b0e2cb0c8c3592
ushers956 <ushershaninz@gmail.com>
2024-09-17 18:04:57 +1200

    birthday

---
+++ birthday.py
@@ -0,0 +1,12 @@
+print("birthday of 5 friends \n")
+#sorting birthdays in variables and printing them
+a="31/12/13"
+print("jethro -",a)
+b="31/12/12"
+print("\ndylan -", b)
+c="28/06/12"
+print("\nryan -", c)
+D="27/09/12"
+print("\nfletcher -", D)
+e="22/11/12"
+print("\nkwayd -", e)
